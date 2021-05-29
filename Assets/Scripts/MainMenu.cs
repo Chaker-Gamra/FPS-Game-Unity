10d7978c8cb08a511c352f34d6a82cefcd4c385c
Chaker-Gamra <developer3.5pro@gmail.com>
2021-05-29 08:43:52 +0100

    Start Menu in Unity

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
